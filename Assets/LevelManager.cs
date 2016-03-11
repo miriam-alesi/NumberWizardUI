bc12d62841806f68c90952edf129d6b56713f72f
miriam-alesi <miriam@purplemaia.org>
2016-03-11 12:48:12 -1000

    Commit completed game

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void  LoadLevel (string name){
+		Debug.Log ("Level load requested for " + name);
+		Application.LoadLevel(name);	
+			
+		}
+	
+	public void QuitRequest (string name){
+		Debug.Log ("I want to quit!");
+		Application.Quit();
+
+	}
+	public void ReturnToStart(string name)
+	{
+	Application.LoadLevel(name);
+	}
+}
